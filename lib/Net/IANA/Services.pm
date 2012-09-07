060d8b53b188fac05fb566d3c6276a9e38b311c6
Adam Lesperance <lespea@gmail.com>
2012-09-06 22:04:58 -0500

    New module skeleton

---
+++ lib/Net/IANA/Services.pm
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+use utf8;
+
+package Net::IANA::Services;
+
+#ABSTRACT:  Main module for Net::IANA::Services
+
+#  This is the 21st century
+use 5.010;
+
+#  Import modules
+
+#  Common modules
+use autodie;
+use Data::Dumper;
+
+
+=encoding utf8
+
+=for Pod::Coverage
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+
+#  Happy ending
+1;
